fc8ac03761891cad74b642d82c2a113022b7a5b2
alfredo <alfredo.arredondo@hotmail.com>
2022-11-04 10:35:58 -0300

    2022110402

--- assets/js/appPassword.js
+++ assets/js/appPassword.js
@@ -4,9 +4,9 @@ const ValueOne = document.querySelector("#ValueOne");
 const ValueTwo = document.querySelector("#ValueTwo");
 const ValueThree = document.querySelector("#ValueThree");
 boton.addEventListener("click", () => {
-    if ( ValueOne.value == 9 && ValueTwo.value == 1 && ValueThree.value == 1) {
+    if ( ValueOne.value === 9 && ValueTwo.value === 1 && ValueThree.value === 1) {
         titlePassword.textContent = "Password 1 Correcta!";
-    } else if (ValueOne.value == 7 && ValueTwo.value == 1 && ValueThree.value == 4) {
+    } else if (ValueOne.value === 7 && ValueTwo.value === 1 && ValueThree.value === 4) {
         titlePassword.textContent = "Password 2 Correcta!";
     } else {
         titlePassword.textContent = "Password Incorrecta!";
