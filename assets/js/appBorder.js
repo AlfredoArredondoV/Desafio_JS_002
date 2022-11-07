cfbaad6ba0103f8ec526e169ed5a5422b09b266f
alfredo <alfredo.arredondo@hotmail.com>
2022-11-07 19:56:28 -0300

    2022110702

--- assets/js/appBorder.js
+++ assets/js/appBorder.js
@@ -1,6 +1,6 @@
 const border = document.querySelector("#picture");
 border.addEventListener("click", () => {
-    if ( document.getElementById("picture").style.border == "" ) {
+    if ( document.getElementById("picture").style.border === "" ) {
         document.getElementById("picture").style.border = "2px solid red";
     } else {
         document.getElementById("picture").style.border = "";
